c7837f07d3a08d6e06ea632e19064226ba1aba02
Tara Parker <tara.parker@ttu.edu>
2014-04-01 17:14:34 -0500

    Fixing github repo pt2

---
+++ register_success.php
@@ -0,0 +1,21 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Tara
+ * Date: 3/19/14
+ * Time: 1:55 PM
+ */
+?>
+
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta charset="UTF-8">
+        <title>Super Sleep: Registration Success</title>
+        <link rel="stylesheet" href="styles/main.css" />
+    </head>
+    <body>
+        <h1>Registration successful!</h1>
+        <p>You can now go back to the <a href="index.php">login page</a> and log in</p>
+    </body>
+</html>
