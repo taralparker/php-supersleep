70df3024e9e49418f58fda9c645299d1793e186f
mattdw1 <mxmatrix@msn.com>
2014-04-02 18:11:33 -0500

    journal submission

---
+++ data_entry.php
@@ -0,0 +1,169 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Matt
+ * Template: Tara
+ * Date: 4/1/14
+ * Time: 6:00 PM
+ */
+
+include_once 'includes/db_connect.php';
+include_once 'includes/functions.php';
+
+sec_session_start();
+?>
+<!DOCTYPE html>
+<html>
+<head>
+    <meta charset="UTF-8">
+    <title>Super Sleep Journal</title>
+    <script type="text/JavaScript" src="js/forms.js"></script>
+    <link rel="stylesheet" href="styles/main.css" />
+</head>
+<body>
+<?php if (login_check($mysqli) == true) : ?>
+    <html>
+		<head>
+			<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+			<meta charset="UTF-8" />
+			<meta name="google" content="notranslate">
+			<meta http-equiv="Content-Language" content="en" />
+			<title>Super Sleep</title>
+
+			<script type="text/javascript" src="jquery-1.7.1.min.js"></script>
+
+			<link href="http://fonts.googleapis.com/css?family=Open+Sans:400,300,600|Archivo+Narrow:400,700" rel="stylesheet" type="text/css" />
+			<link href="default.css" rel="stylesheet" type="text/css" media="all" />
+
+		</head>
+		<body>
+			<div id="wrapper" class="container">
+				<div id="header">
+					<div id="logo">
+						<h1><a href="#">Super Sleep</a></h1>
+						<span><p>Welcome <?php echo htmlentities($_SESSION['username']); ?>!</p></span>
+					</div>
+				</div>
+				<div id="page">
+					<div id="content">
+									
+						<form action="data_submit.php" method="post">
+							Month: 
+							<select id="month" name="month">
+									<option value="january">January</option>
+									<option value="february">February</option>
+									<option value="march">March</option>
+									<option value="april">April</option>     
+									<option value="may">May</option>     
+									<option value="june">June</option>     
+									<option value="july">July</option>     
+									<option value="august">August</option>     
+									<option value="september">September</option>     
+									<option value="october">October</option>     
+									<option value="november">November</option>     
+									<option value="december">December</option>     
+							</select>    
+								
+							<br>
+							<!-- needs to check how many days in the month -->
+							Day: 
+							<select id="day" name="day">  
+								<?php
+									for ($day=1; $day<=100; $day++)
+									{
+										?>
+											<option value="<?php echo $day;?>"><?php echo $day;?></option>
+										<?php
+									}
+								?>
+							</select>
+							<br>
+							Year: 
+							<select id="year" name="year">
+								<?php
+									for ($year=2014; $year>=1980; $year--)
+									{
+										?>
+											<option value="<?php echo $year;?>"><?php echo $year;?></option>
+										<?php
+									}
+								?>
+							</select>
+							<br>
+							Sleep Start Time:
+							<br>
+							Time: 
+							<select id="hour" name="hour">
+								<?php
+									for ($hour=12; $hour>=1; $hour--)
+									{
+										?>
+											<option value="<?php echo $hour;?>"><?php echo $hour;?></option>
+										<?php
+									}
+								?>
+							</select>
+							
+							: 
+							<select id="minute" name="minute">
+								<?php
+									for ($minute=0; $minute<60; $minute+=5)
+									{
+										?>
+											<option value="<?php echo $minute;?>"><?php echo $minute;?></option>
+										<?php
+									}
+								?>
+							</select>
+							
+							<select id="ampm" name="ampm">
+								<option value="pm">PM</option>
+								<option value="am">AM</option>
+							</select>
+							<br>
+							Hours Slept: 
+							<select id="hoursslept" name="hoursslept">
+								<?php
+									for ($hoursslept=.25; $hoursslept<=24; $hoursslept+=.25)
+									{
+										?>
+											<option value="<?php echo $hoursslept;?>"><?php echo $hoursslept;?></option>
+										<?php
+									}
+								?>
+							</select>
+							<br>
+							<textarea rows="4" cols="50" maxlength="300" id="journal"name = "journal">Sleep Comments (limit 300 characters)</textarea>
+							<br>
+							<br>
+							<br>
+							<input type="submit"/>
+						</form>
+					</div>
+					<div id="sidebar">
+						<div>
+							<h2>Menu</h2>
+							<ul class="style1">
+								<li class="first"><a href="#">New Data</a></li>
+								<li><a href="#">View Statistics</a></li>
+								<li><a href="#">View Graphs</a></li>
+								<li><a href="#">Post to Community Thread</a></li>
+								<li><a href="includes/logout.php">Logout</a></li>
+							</ul>
+
+						</div>
+					</div>
+				</div>
+				<div id="footer">
+					<p>Developed by Tara Parker, Christopher Reynolds, Matthew Webster. Design by FreeCSSTemplates.org.
+				</div>
+			</div>
+		</body>
+    </html>
+<?php else : ?>
+    <p>
+        <span class="error">You are not authorized to access this page.</span> Please <a href="index.php">login</a>.
+    </p>
+<?php endif; ?>
+</body>
+</html>
---
+++ data_submit.php
@@ -0,0 +1,69 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Matt
+ * Date: 4/2/14
+ * Time: 3:00 PM
+ */
+
+include_once 'includes/db_connect.php';
+include_once 'includes/functions.php';
+
+sec_session_start(); // Our custom secure way of starting a PHP session.
+
+//if logged in
+if (login_check($mysqli) == true)
+{
+	$month = $_POST['month'];
+	echo $month;
+
+
+
+
+	if (isset($_POST['month'], $_POST['day'], $_POST['year'], $_POST['hour'], $_POST['minute'], $_POST['ampm'], $_POST['hoursslept'], $_POST['journal']))
+	{
+		$month = filter_input(INPUT_POST, 'month', FILTER_SANITIZE_STRING);
+		$day = filter_input(INPUT_POST, 'day', FILTER_SANITIZE_STRING);
+		$year = filter_input(INPUT_POST, 'year', FILTER_SANITIZE_STRING);
+		$hour = filter_input(INPUT_POST, 'hour', FILTER_SANITIZE_STRING);
+		$minute = filter_input(INPUT_POST, 'minute', FILTER_SANITIZE_STRING);
+		$ampm = filter_input(INPUT_POST, 'ampm', FILTER_SANITIZE_STRING);
+		$hoursslept = filter_input(INPUT_POST, 'hoursslept', FILTER_SANITIZE_STRING);
+		$journal = filter_input(INPUT_POST, 'journal', FILTER_SANITIZE_STRING);
+		$username = htmlentities($_SESSION['username']);
+
+		//if journal is equal to "Sleep Comments"
+		if (strcasecmp($journal,"Sleep Comments")==0)
+		{
+			//clear journal if text is default
+			$journal = "";
+		}
+		//if the journal is more then 300 characters, cut it short (No buffer overflows for us! ha!)
+		if(strlen($journal) > 300)
+		{
+			$journal = substr($journal, 0,300);
+		}
+		// Insert the new entry into the database
+		if ($insert_stmt = $mysqli->prepare("INSERT INTO sleep_data (username, month, day, year, hour, min, ampm, hoursSlept, comment) VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?)"))
+		{
+			$insert_stmt->bind_param('sssssssss', $username, $month, $day, $year, $hour, $minute, $ampm, $hoursslept, $journal);
+			// Execute the prepared query.
+			if (! $insert_stmt->execute())
+			{
+				header('Location: ../error.php?err=Journal failure: INSERT');
+			}
+		}
+		header('Location: ../account.php');
+	}
+	else
+	{
+		// The correct POST variables were not sent to this page.
+		echo 'Invalid Request';
+	}
+}
+?>
+<?php if (login_check($mysqli) == false) : ?>
+    <p>
+        <span class="error">You are not authorized to access this page.</span> Please <a href="index.php">login</a>.
+    </p>
+<?php endif; ?>
