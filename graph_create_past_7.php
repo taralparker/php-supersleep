b02766728ceee5c13825571a58aca6b131eef529
Tara Parker <tara.parker@ttu.edu>
2014-04-19 20:21:54 -0500

    Comment fix

--- graph_create_past_7.php
+++ graph_create_past_7.php
@@ -7,8 +7,6 @@
  */
 
 	include "/libchart/classes/libchart.php";
-    //include "/includes/db_connect.php";
-    //include "/includes/functions.php";
 
     $array = array();
     $query = "SELECT CONCAT(month, '/', day, '/', year), hoursSlept FROM sleep_data WHERE username='$_SESSION[username]' ORDER BY CONCAT(year, month, day) DESC LIMIT 7";
@@ -29,15 +27,3 @@ $chart = new LineChart();
 	$chart->render("generated_graphs/past7entries.png");
 ?>
 
-<!--
-<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
-<html xmlns="http://www.w3.org/1999/xhtml">
-<head>
-    <title>Libchart line demonstration</title>
-    <meta http-equiv="Content-Type" content="text/html; charset=ISO-8859-15" />
-</head>
-<body>
-<img alt="Line chart" src="generated_graphs/past7entries.png" style="border: 1px solid gray;"/>
-</body>
-</html>
--->
