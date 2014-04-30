6db68106c7ada5921b2a6c6ea60c872d789a23f6
Crystomadness <Christopher7201@att.net>
2014-04-30 16:41:32 -0500

    Fixed Past Seven Entries
    Changed the mechanism used to collect the past seven entries in the
    sleep_data table. Much more streamlined as we are only collecting the
    past seven entries in the table and not the entries in the past seven
    days.

--- view_statistics_past_seven.php
+++ view_statistics_past_seven.php
@@ -14,38 +14,11 @@ sec_session_start();
 </head>
 <body>
 <?php
-    //If the user is logged in, then the constraint is set to be the past seven days. The user is then redirected to
+    //If the user is logged in, then the constraint is set to be the past seven entries. The user is then redirected to
     //"view_statistics.php", where the constraint will be used to query the database.
     if (login_check($mysqli) == true) :
-        $month = date("m");
-        $day = date("j");
-        $year = date("Y");
-
-        $months = array();
-        $days = array();
-        $years = array();
-
-        $dateinterval = new DateInterval('P1D');
-        $date = new DateTime();
-        $date->setDate($year,$month,$day);
-        //Set the date for the current day, and the date interval for 1 day.
-
-        for($i=0;$i<7;$i++)
-        {
-            //Subtract 1 day from the date and put the components of the result into the three designated arrays.
-            $date->sub($dateinterval);
-            $months[$i] = $date->format('m');
-            $days[$i] = $date->format('j');
-            $years[$i] = $date->format('Y');
-        }
-
-        //The arrays are then indexed and used in these statements that will become part of the constraint.
-        $monthcheck = "(month='$months[0]' or month='$months[1]' or month='$months[2]' or month='$months[3]' or month='$months[4]' or month='$months[5]' or month='$months[6]')";
-        $daycheck = "(day='$days[0]' or day='$days[1]' or day='$days[2]' or day='$days[3]' or day='$days[4]' or day='$days[5]' or day='$days[6]')";
-        $yearcheck = "(year='$years[0]' or year='$years[1]' or year='$years[2]' or year='$years[3]' or year='$years[4]' or year='$years[5]' or year='$years[6]')";
-
-        //The constraint is concatenated together so that only the past seven days will be selected when the database is queried.
-        $_SESSION['constraint'] = "(username='$_SESSION[username]' and " . $monthcheck . " and " . $daycheck . " and " . $yearcheck . " )";
+        //Let the constraint limit the entries to the last seven in the table.
+        $_SESSION['constraint'] = "username='$_SESSION[username]' ORDER BY id DESC LIMIT 7";
         header('Location: ../view_statistics.php');
     else :
 ?>
