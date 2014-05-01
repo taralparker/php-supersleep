0da755e921e60dfca569a7e0f39b8306227f2778
Tara Parker <tara.parker@ttu.edu>
2014-05-01 18:13:48 -0500

    Updated Readme
    Updated readme and .sql

--- README.txt
+++ README.txt
@@ -1,21 +1,12 @@
 README
-for more information on the methods used, go to 
-http://www.wikihow.com/Create-a-Secure-Login-Script-in-PHP-and-MySQL
 
-1.
-Delete super_sleep database in MySQL OR SKIP STEP a).
+If you already have the database 'super_sleep', delete all tables in the 'super_sleep' database. Then skip to 2.(NOTE: If you do not remove the current tables, the updates will not make it into the database)
 
-2.
-Go to SQL page where you can Run SQL query/queries on server.
-
-3.
-Run these queries in order, one at a time.
-
+If you do not have a database named 'super_sleep', then first run a, then b, in http://localhost/phpmyadmin/  Then proceed to 2. 
+    
 
 a)
-
 CREATE DATABASE `super_sleep`;
-
 
 
 b) create database user account
@@ -24,50 +15,21 @@ CREATE USER 'sec_user'@'localhost' IDENTIFIED BY 'L6S5rMtnQG4xkUm3';
 GRANT SELECT, INSERT, UPDATE, DELETE ON `super_sleep`.* TO 'sec_user'@'localhost';
 
 
-
-c) 
-
-CREATE TABLE `super_sleep`.`members` (
-    `id` INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
-    `username` VARCHAR(30) NOT NULL,
-    `email` VARCHAR(50) NOT NULL,
-    `password` CHAR(128) NOT NULL,
-    `salt` CHAR(128) NOT NULL 
-) ENGINE = InnoDB;
+2. Import the given .sql by navigating to Import in http://localhost/phpmyadmin/, then browsing for the given .sql file. Leave all settings as defaul, then press Go. 
 
 
+LOGINS:
+User: demo@demo.com
+Password: Demo1!
 
-d) stores login attempts for a user
-
-CREATE TABLE `super_sleep`.`login_attempts` (
-    `user_id` INT(11) NOT NULL,
-    `time` VARCHAR(30) NOT NULL
-) ENGINE=InnoDB
+Is a common practice user with some data. 
 
 
+User: graph@graph.com
+User: Graph1!
 
-e) create a test row with known details
-Test User-
-Username: test_user
-Email: test@example.com
-Password: 6ZaxN2Vzm9NUJT2y
+This user has data, but much less.
+
+Feel free to register your own username and create new data. 
 
 
-INSERT INTO `super_sleep`.`members` VALUES(1, 'test_user', 'test@example.com',
-'00807432eae173f652f2064bdca1b61b290b52d40e429a7d295d76a71084aa96c0233b82f1feac45529e0726559645acaed6f3ae58a286b9f075916ebf66cacc',
-'f9aab579fc1b41ed0c44fe4ecdbfcdb4cb99b9023abb241a6db833288f4eea3c02f76e0d35204a8695077dcf81932aa59006423976224be0390395bae152d4ef');
-
-f) add table to store sleep data
-
-CREATE TABLE `sleep_data` (
-    `id` INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
-    `username` VARCHAR(30) NOT NULL,
-    `month` VARCHAR(10) NOT NULL,
-    `day` INT NOT NULL,
-    `year` INT NOT NULL,
-    `hour` VARCHAR(30) NOT NULL,
-    `min` VARCHAR(30) NOT NULL,
-    `ampm` VARCHAR(2) NOT NULL,
-    `hoursSlept` INT NOT NULL, 
-    `comment` VARCHAR(300)
-) ENGINE = InnoDB;
--- super_sleep.sql
+++ super_sleep.sql
@@ -3,7 +3,7 @@
 -- http://www.phpmyadmin.net
 --
 -- Host: 127.0.0.1
--- Generation Time: Apr 30, 2014 at 11:55 PM
+-- Generation Time: May 02, 2014 at 01:12 AM
 -- Server version: 5.6.16
 -- PHP Version: 5.5.9
 
@@ -101,7 +101,11 @@ INSERT INTO `login_attempts` (`user_id`, `time`) VALUES
 (7, '1396312001'),
 (7, '1396312006'),
 (7, '1397956555'),
-(7, '1398893249');
+(7, '1398893249'),
+(2, '1398983670'),
+(2, '1398985783'),
+(2, '1398985792'),
+(4, '1398985845');
 
 -- --------------------------------------------------------
 
@@ -116,7 +120,7 @@ CREATE TABLE IF NOT EXISTS `members` (
   `password` char(128) NOT NULL,
   `salt` char(128) NOT NULL,
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=8 ;
+) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=9 ;
 
 --
 -- Dumping data for table `members`
@@ -125,11 +129,9 @@ CREATE TABLE IF NOT EXISTS `members` (
 INSERT INTO `members` (`id`, `username`, `email`, `password`, `salt`) VALUES
 (1, 'test_user', 'test@example.com', '00807432eae173f652f2064bdca1b61b290b52d40e429a7d295d76a71084aa96c0233b82f1feac45529e0726559645acaed6f3ae58a286b9f075916ebf66cacc', 'f9aab579fc1b41ed0c44fe4ecdbfcdb4cb99b9023abb241a6db833288f4eea3c02f76e0d35204a8695077dcf81932aa59006423976224be0390395bae152d4ef'),
 (2, 'test', 'test@test.com', 'ae97ad4806d7fe3f9a15c9942bc15be26c6488672469f13c02e9719144024e0ddaceca7dda33ff32c73dbb9181f708ebe1ac598f997e3999922291cc6d8e687e', '5cdcd55207f7f2ad800241ab27c76cacac0704e1c411bbefc167caa19826538fdafae50e625e5253c944b4e84ba82e1131dd9f20cfc2ebb527f3df3aadb928dc'),
-(3, 'Test', 'test123@test.com', '1a6a256deb07f242e117abac2b8cc2a813ada3e2286a85f7201c9a9df63e6012f082aa156990a0abbb5bb94fb5a3539983694d422631967416919c57f670e921', 'f5deecaa3311660d5a09932dc5377a73757c4f709a4be2fffeda4f08039da21d2fed9a8adab4bf953932c00cf9103ebace894ba1effe3ae0f10169de0f857b97'),
 (4, 'Tara', 'tara@tara.com', 'f05ef78fa8ef62d06a56e5e6447e1323e14810bc9d5bf93f82a36a0fed02e444c1362677e87f3b0de8d5a3b3c6227150d089720956fb6cb4074633f78ec792a4', '6d427d7d72aedc5943d3985286412fe0648be5750fccb54cf77660568cc7ef5a4dc0032af47276cd9ea298eca2c300c09b2a66039ec21a7fa1399a6ed6a8f3c3'),
-(5, 'Jordan', 'jor@jor.com', 'e62554e352801234526d3562d34ccdceac43edde89d1682ae6f2b91d15d7770f26fc77f8e616697cec2ff99b1bcca5df9252a9dc37507d012f5cb51ab74757e2', '73612c9d16d05e5f94f0adeeabd53d080a8be4d5c763c7b393ef63fff25e482da43eeba61d97d0c4b2857059cd72f8228951eba90640470e1037590810628b93'),
-(6, 'Chris', 'Chris@chris.com', '5ab61a6313e484d79fcf296f9f70434aa9ac7735c7aa8e8d1fdc4159278aa9ca8b76b5efa95d03dfd3183c2957b655d566b19d56ddfa63e6684d72d6f3a54fd1', 'f3c3ef8bb742020ed4dfd80530339b57cab70a341a1d4a85422bd99ea31ed2d52cb3097f39ede83ec78f284a0106391083ef184f037749a02390b26511eb6018'),
-(7, 'demo', 'demo@demo.com', '6467c07d7344c3f84ea401c107348dc6d6677e451e2be6d85935a82f1ab98adc1e2020d7ca1c767dff317d68ee8e5544a8ab5d593122692c141f74d196cc50be', 'd97479ff32d0e1afe932bdbd9e04fa000eb18b05ddbc628119a1f73f8e5957be8828c0046dc7c6fc2412fe4761336c0ab28ab7c3caa643a2500583fd967541b5');
+(7, 'demo', 'demo@demo.com', '6467c07d7344c3f84ea401c107348dc6d6677e451e2be6d85935a82f1ab98adc1e2020d7ca1c767dff317d68ee8e5544a8ab5d593122692c141f74d196cc50be', 'd97479ff32d0e1afe932bdbd9e04fa000eb18b05ddbc628119a1f73f8e5957be8828c0046dc7c6fc2412fe4761336c0ab28ab7c3caa643a2500583fd967541b5'),
+(8, 'Graph', 'Graph@graph.com', '0a674ab07ae4bd955fa0b76aa62fd8aa8956f4ea7c097e72cf229f55cdcfb2c4d581e9dd0c94174b9d991150dc3591ef85e9680d02c6a1c11cc7caf775588b69', 'f97d96c20871ebb2532d8972d95e06f20bd6bf4fafd2e0bb4df2014d02776d2c28fc889e40d5cfcd12be16ab3fc2e61237be0554004251b4c054da123ac9f029');
 
 -- --------------------------------------------------------
 
@@ -164,7 +166,7 @@ CREATE TABLE IF NOT EXISTS `sleep_data` (
   `hoursSlept` float NOT NULL,
   `comment` varchar(255) NOT NULL,
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=49 ;
+) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=61 ;
 
 --
 -- Dumping data for table `sleep_data`
@@ -200,7 +202,19 @@ INSERT INTO `sleep_data` (`id`, `username`, `month`, `day`, `year`, `hour`, `min
 (45, 'demo', 7, 13, 2014, 12, 0, 'pm', 3, 'Sleep Comments (limit 300 characters)'),
 (46, 'demo', 9, 10, 2014, 12, 0, 'pm', 2, 'Sleep Comments (limit 300 characters)'),
 (47, 'demo', 11, 8, 2014, 12, 0, 'pm', 1, 'Sleep Comments (limit 300 characters)'),
-(48, 'demo', 9, 14, 2014, 12, 0, 'pm', 8, 'Sleep Comments (limit 300 characters)');
+(48, 'demo', 9, 14, 2014, 12, 0, 'pm', 8, 'Sleep Comments (limit 300 characters)'),
+(49, 'Graph', 1, 1, 2014, 12, 0, 'pm', 3.25, ''),
+(50, 'Graph', 1, 19, 2014, 12, 0, 'pm', 0.25, ''),
+(51, 'Graph', 7, 1, 2007, 12, 0, 'pm', 2.25, ''),
+(52, 'Graph', 6, 15, 2004, 12, 0, 'pm', 3, ''),
+(53, 'Graph', 2, 25, 2014, 12, 0, 'pm', 0.25, ''),
+(54, 'Graph', 9, 9, 2014, 12, 0, 'pm', 13.75, ''),
+(55, 'Graph', 1, 1, 2014, 12, 0, 'pm', 5, ''),
+(56, 'Graph', 8, 1, 2013, 12, 0, 'pm', 3.5, ''),
+(57, 'Graph', 4, 3, 2014, 12, 0, 'pm', 2.25, ''),
+(58, 'Graph', 4, 2, 2014, 12, 0, 'pm', 8, ''),
+(59, 'Graph', 4, 4, 2014, 12, 0, 'pm', 14.75, ''),
+(60, 'Graph', 4, 30, 2014, 10, 0, 'pm', 5, '');
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
 /*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
