e441a2b1df209ec45327dc4258fa921f30d366f3
Tara Parker <tara.parker@ttu.edu>
2014-05-01 17:45:10 -0500

    Graph Date Ordering
    Fixed date ordering

--- graph_create_past_7.php
+++ graph_create_past_7.php
@@ -9,7 +9,9 @@
 	include "/libchart/classes/libchart.php";
 
     $array = array();
-    $query = "SELECT CONCAT(month, '/', day, '/', year), hoursSlept FROM sleep_data WHERE username='$_SESSION[username]' ORDER BY CONCAT(year, month, day) DESC LIMIT 7";
+    //SQL query to get last 7 entries of sleep time data
+    $query = "SELECT CONCAT(month, '/', day, '/', year), hoursSlept FROM sleep_data WHERE username='$_SESSION[username]' ORDER BY year DESC, month DESC, day DESC LIMIT 7";
+    //$query = "SELECT CONCAT(month, '/', day, '/', year), hoursSlept FROM sleep_data WHERE username='$_SESSION[username]' ORDER BY CONCAT(year, month, day) DESC LIMIT 7";
     $result = $mysqli->query($query);
 
 
@@ -17,12 +19,14 @@ $chart = new LineChart();
 
 	$dataSet = new XYDataSet();
 
+    //Add a point on the graph for each of the seven days
     while ($row = $result->fetch_array(MYSQLI_NUM)) {
         $dataSet->addPoint(new Point($row[0], $row[1]));
     }
 
 	$chart->setDataSet($dataSet);
 
+    //Generate and save the chart.
 	$chart->setTitle("Sleep");
 	$chart->render("generated_graphs/past7entries.png");
 ?>
