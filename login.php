fab07d9ec71b755469b1502ace8c3efa995ff522
Tara Parker <tara.parker@ttu.edu>
2014-05-08 18:19:11 -0500

    Fixed Login Template
    Placed Login and Login_fail in the template to match the rest of the
    site.

--- login.php
+++ login.php
@@ -1,9 +1,10 @@
+
 <?php
 /**
  * Created by PhpStorm.
  * User: Tara
- * Date: 3/19/14
- * Time: 1:54 PM
+ * Date: 5/8/14
+ * Time: 6:03 PM
  */
 
 include_once 'includes/db_connect.php';
@@ -20,33 +21,73 @@ else
     $logged = 'out';
 }
 ?>
-<!DOCTYPE html>
-<html>
+
 <head>
+    <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+    <meta charset="UTF-8" />
+    <meta name="google" content="notranslate">
+    <meta http-equiv="Content-Language" content="en" />
     <title>Super Sleep: Log In</title>
-    <link rel="stylesheet" href="styles/main.css" />
+
+    <script type="text/javascript" src="jquery-1.7.1.min.js"></script>
+
+    <link href="http://fonts.googleapis.com/css?family=Open+Sans:400,300,600|Archivo+Narrow:400,700" rel="stylesheet" type="text/css" />
+    <link href="default.css" rel="stylesheet" type="text/css" media="all" />
+
     <script type="text/JavaScript" src="js/sha512.js"></script>
     <script type="text/JavaScript" src="js/forms.js"></script>
+
 </head>
 <body>
-<?php
-if (isset($_GET['error']))
-{
-    echo '<p class="error">Error Logging In!</p>';
-}
-?>
-<form action="includes/process_login.php" method="post" name="login_form">
-    Email: <input type="text" name="email" />
-    Password: <input type="password"
-                     name="password"
-                     id="password"/>
-    <input type="button"
-           value="Login"
-           onclick="formhash(this.form, this.form.password);" />
-</form>
-<p>If you don't have a login, please <a href="register.php">register</a></p>
-<p>If you are done, please <a href="includes/logout.php">log out</a>.</p>
-<p>You are currently logged <?php echo $logged ?>.</p>
+<div id="wrapper" class="container">
+    <div id="header">
+        <div id="logo">
+            <h1><a href="#">Super Sleep</a></h1>
+            <span>Get the sleep you deserve</span>
+        </div>
+    </div>
+
+
+    <div id="page">
+        <div id="content">
+            <h2>Login</h2>
+
+            <?php
+            if (isset($_GET['error']))
+            {
+                echo '<p class="error">Error Logging In!</p>';
+            }
+            ?>
+            <form action="includes/process_login.php" method="post" name="login_form">
+                Email:     <input type="text" name="email" />
+                Password: <input type="password"
+                                 name="password"
+                                 id="password"/>
+                <input type="button"
+                       value="Login"
+                       onclick="formhash(this.form, this.form.password);" />
+            </form>
+            <p>If you don't have a login, please <a href="register.php">register</a></p>
+            <p>If you are done, please <a href="includes/logout.php">log out</a>.</p>
+            <p>You are currently logged <?php echo $logged ?>.</p>
 </body>
 </html>
+</div>
+<div id="sidebar">
+    <div>
+        <h2>Improve your sleep</h2>
+        <ul class="style1">
+            <li class = first><a href="register.php">Register</a></li>
+            <li><a href='contact.php'>Contact Us</a></li>
+        </ul>
 
+    </div>
+</div>
+</div>
+</div>
+<div id="footer">
+    <p>Developed by Tara Parker, Christopher Reynolds, Matthew Webster. Design by FreeCSSTemplates.org.
+</div>
+</div>
+</body>
+</html>
--- login_fail.php
+++ login_fail.php
@@ -3,24 +3,65 @@
  * Created by PhpStorm.
  * User: Tara
  * Date: 3/19/14
- * Time: 2:37 PM
+ * Time: 3:33 PM
  */
 
 ?>
-
 <html>
+<!--
+Design by Free CSS Templates
+http://www.freecsstemplates.org
+Released for free under a Creative Commons Attribution 2.5 License
+
+Name       : Portraiture
+Description: A two-column, fixed-width design with dark color scheme.
+Version    : 1.0
+Released   : 20130111
+-->
+
 <head>
-    <title>Super Sleep</title>
-    Login Failed!
-    <!--- Convey that the login failed with the submitted parameters. --->
+    <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+    <meta charset="UTF-8" />
+    <meta name="google" content="notranslate">
+    <meta http-equiv="Content-Language" content="en" />
+    <title>Super Sleep: Login Failed</title>
+
+    <script type="text/javascript" src="jquery-1.7.1.min.js"></script>
+
+    <link href="http://fonts.googleapis.com/css?family=Open+Sans:400,300,600|Archivo+Narrow:400,700" rel="stylesheet" type="text/css" />
+    <link href="default.css" rel="stylesheet" type="text/css" media="all" />
+
 </head>
-
 <body>
+<div id="wrapper" class="container">
+    <div id="header">
+        <div id="logo">
+            <h1><a href="#">Super Sleep</a></h1>
+            <span>Get the sleep you deserve</span>
+        </div>
+    </div>
 
-<br>
-<br>
-<!--- Indicate that one or both of these parameters was the issue. --->
-Please try again. <a href="login.php">Login</a>
 
+    <div id="page">
+        <div id="content">
+            <h2>Login Failed</h2>
+            <p>Please try again. <a href="login.php">Login</a></p>
+        </div>
+        <div id="sidebar">
+            <div>
+                <h2>Improve your sleep</h2>
+                <ul class="style1">
+                    <li class = first><a href="register.php">Register</a></li>
+                    <li><a href='contact.php'>Contact Us</a></li>
+                </ul>
+
+            </div>
+        </div>
+    </div>
+    <div id="footer">
+        <p>Developed by Tara Parker, Christopher Reynolds, Matthew Webster. Design by FreeCSSTemplates.org.
+    </div>
+</div>
 </body>
 </html>
+
